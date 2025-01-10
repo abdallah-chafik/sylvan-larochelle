e1fb22cd518d660ed5cd07fbc15da836498d3d6c
abdallah-chafik <chafik_abd@upf.ac.ma>
2025-01-10 17:26:34 +0100

    Course model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Course(models.Model):
+    course_name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="courses")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.course_name
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,30 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Course
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        feilds = ["id", 'username', 'password']
+        fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {'write_only': True, 'min_length': 8},
+            'id': {'read_only': True}
         }
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    def create(self, validated_data):
+        # Create user with encrypted password
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('id', 'course_name', 'author', 'author', 'created_at')
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'author': {'read_only': True}
+        }
